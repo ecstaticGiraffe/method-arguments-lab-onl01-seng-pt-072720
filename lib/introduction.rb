355c282d72cdfa87dcd77dcbb75783b2a79c8588
C. Luke Murphree <clukemurphree@icloud.com>
2020-08-06 03:42:21 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,16 @@
+def introduction(name)
+  puts (name)
+end
+
+name="Luke"
+
+introduction
+
+def introduction_with_language(name, language)
+end
+
+introduction_with_language
+
+def introduction_with_language(name, language)
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
